3ef898ddf776015d397bb694b08ec3c09e75f1d5
RodrigoHernandez09 <rodrigomhr@icloud.com>
2026-02-26 17:01:30 -0500

    Add files via upload

---
+++ DivisionAndConversionExceptions.cs
@@ -0,0 +1,41 @@
+using System;
+class DivisionConversionExceptions {
+	static void Main() {
+		//Asking for the first and second number as strings
+		Console.WriteLine("What is the first number?");
+		string first = Console.ReadLine();
+
+		Console.WriteLine("What is the second number?");
+		string second = Console.ReadLine();
+
+		try {
+			int firstNumber = Convert.ToInt32(first);
+			int secondNumber = Convert.ToInt32(second);
+			int division = Divide(firstNumber,secondNumber);
+			Console.WriteLine("The result of " + firstNumber + " divided by " + secondNumber + " is: " + division);
+		}
+		catch(DivideByZeroException zero) {
+			Console.WriteLine("You can not divide by zero.");
+			Console.WriteLine("Error message: " + zero.Message);
+		}
+		catch(FormatException form) {
+			Console.WriteLine("You need to enter a number.");
+			Console.WriteLine("Error message: " + form.Message);
+		}
+		catch(InvalidCastException cast) {
+			Console.WriteLine("The number(s) entered was not an integer.");
+			Console.WriteLine("Error message: " + cast.Message);
+		}
+		catch(OverflowException over) {
+			Console.WriteLine("The number(s) entered was too big.");
+			Console.WriteLine("Error message: " + over.Message);
+		}
+		catch(Exception e) {
+			Console.WriteLine("Sorry an unexpected error has occured.");
+			Console.WriteLine("Error message: " + e.Message);
+		}
+	}
+	static int Divide(int a,int b) {
+		return a / b;
+	}
+}
